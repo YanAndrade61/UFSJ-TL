89803963f64918985c5e053e7f90e76dfb665899
yan <yanccomp@gmail.com>
2023-06-05 18:12:31 -0300

    automaton finish

--- afdn-simulator/automaton.py
+++ afdn-simulator/automaton.py
@@ -3,3 +3,22 @@ class automaton:
         self.start = config['start']
         self.ends = set(config['ends'])
         self.rules = config['rules']
+    
+    def process(self, string: str):
+        
+        states_on = set([self.start])
+        
+        for symbol in string:
+
+            #Update the active states
+            states = set()
+            for s in states_on:
+                states |= self.next_state(s,symbol)
+                               
+            states_on = states
+
+        print('BELEZEIRA') if len(states_on & self.ends) > 0 else print('Nao aceita')
+        
+
+    def next_state(self,state: str, symbol: str):
+        return set(self.rules[state].get(symbol,[]))
--- afdn-simulator/main.py
+++ afdn-simulator/main.py
@@ -2,9 +2,9 @@ import yaml
 from automaton import automaton
 
 if __name__ == '__main__':
-    with open("config.yaml", "r") as f:
+    with open("examples/config2.yaml", "r") as f:
         config = yaml.safe_load(f)
     x = automaton(config)
-    print(x.start)
-    print(x.ends)
-    print(x.rules)
+    palavras = ['abab','abbaa','b','aaaa','abba']
+    for s in palavras:
+        x.process(s)
